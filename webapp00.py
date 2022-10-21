33d10a9b80ab479b284ef86f2524cbaeb437e483
vitorsom <116382567+vitorsom@users.noreply.github.com>
2022-10-21 16:16:17 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SENAI ZERBINI É NOTA 1000!")
+st.title("Igreja do Evanjelho Quadrangular")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Abacaxi em tudo!")
+st.header("Jesus salva, cura. liberta e ele voltará!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Bem vindos!")
+st.subheader("Apaz do senhor")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("ande sempre em vigilancia pois não saibais o dia e nem a hora que jesus irá  voltar! ")
